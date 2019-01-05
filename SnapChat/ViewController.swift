52f2fe201a67a0972f6175c6399eaafc69bcf330
Morgan Hondros <morganhondros@gmail.com>
2019-01-05 15:24:16 -0500

    First Commit

---
+++ SnapChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapChat
+//
+//  Created by Morgan Hondros on 1/5/19.
+//  Copyright © 2019 Morgan Hondros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
